7748a5c4b5a3d2af7e8d9dd3b440a600731145b4
mzb527 <mzb527@gmail.com>
2025-06-06 21:04:35 -0400

    Routing and Authentication
    Added authentication features, as well as the basic routing. May be revisited later.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+
+const Navbar = () => {
+  const { user, logout } = useContext(AuthContext);
+
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/watchlist">Watchlist</Link>
+      {user ? (
+        <>
+          <span>Welcome, {user.username}!</span>
+          <button onClick={logout}>Logout</button>
+        </>
+      ) : (
+        <Link to="/login">Login</Link>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
