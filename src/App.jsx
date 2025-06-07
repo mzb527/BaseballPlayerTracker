ee2ef1052b57a08e35a2f06b0be6b2e40b675998
mzb527 <mzb527@gmail.com>
2025-06-06 22:55:09 -0400

    Bug fix
    Code needed to be properly updated.

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,26 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { appRoutes } from "./routes";
+import { AuthProvider } from "./context/AuthContext";
+import { WatchlistProvider } from "./context/WatchlistContext";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./theme";
 
 const App = () => {
   return (
-    <>
-      <Navbar />
-      <Routes>
-        {appRoutes.map(({ path, element }, index) => (
-          <Route key={index} path={path} element={element} />
-        ))}
-      </Routes>
-      <Footer />
-    </>
+    <AuthProvider>
+      <WatchlistProvider>
+        <ThemeProvider theme={theme}>
+          <Navbar />
+          <Routes>
+            {appRoutes.map(({ path, element }, index) => (
+              <Route key={index} path={path} element={element} />
+            ))}
+          </Routes>
+          <Footer />
+        </ThemeProvider>
+      </WatchlistProvider>
+    </AuthProvider>
   );
 };
 
